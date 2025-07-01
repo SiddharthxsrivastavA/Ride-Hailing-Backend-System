2f12a14bd46344846f94cbc7c9d809e6c2977030
Siddharth Srivastava <120250296+SiddharthxsrivastavA@users.noreply.github.com>
2025-07-02 01:44:22 +0530

    ServiceDiscoveryEurekServer

---
+++ ServiceDiscoveryEurekaServer/src/test/java/org/example/servicediscoveryeurekaserver/ServiceDiscoveryEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicediscoveryeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
